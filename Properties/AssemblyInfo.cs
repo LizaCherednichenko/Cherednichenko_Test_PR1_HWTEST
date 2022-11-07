3385b410529b2297a59dd4bfb730d069c7ae191e
u31504 <u31504@P31504>
2022-11-07 11:02:44 +1000

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cherednichenko_Test_PR1_HWTEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cherednichenko_Test_PR1_HWTEST")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d9ba1b6-027d-43a6-bbe4-af8297acee0d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Cherednichenko_Test_PR1_HWTEST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!!";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Cherednichenko_Test_PR1_HW.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }    
+        }
+
+        private const string Expected2 = "Goodbye World!!";
+        [TestMethod]
+        public void TestMethod2()
+        {
+            using (var sw2 = new StringWriter())
+            {
+                Console.SetOut(sw2);
+                Cherednichenko_Test_PR1_HW.Program.SayGoodbye();
+
+                var result2 = sw2.ToString().Trim();
+                Assert.AreEqual(Expected2, result2);
+            }
+        }
+
+    }
+}
